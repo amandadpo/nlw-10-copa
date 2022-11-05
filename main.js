6a1791dcdcaff1c2a09d4b960b031e7f1bb9f606
Amanda de Oliveira Pereira <111541175+amandadpo@users.noreply.github.com>
2022-11-05 13:25:08 -0300

    add main.js

--- main.js
+++ main.js
@@ -23,8 +23,18 @@ function createCard(date, day, games) {
 
 document.querySelector('#cards').innerHTML = 
     createCard("24/11", "quinta", createGame('brazil','16:00', 'serbia')) +
-    createCard("28/11", "segunda", createGame('brazil','13:00', 'switzerland') + createGame('portugal','16:00','uruguay')) +
-    createCard("02/12", "sexta", createGame('brazil','16:00', 'cameroon'))
+    createCard("28/11", "segunda", createGame('brazil','13:00', 'switzerland')) +
+    createCard("02/12", "sexta", createGame('brazil','16:00', 'cameroon'))+
+    createCard("03/12", "oitavas de final", createGame('?','12:00', '?')+ createGame('?','16:00','?'))+
+    createCard("04/12", "oitavas de final", createGame('?','12:00', '?')+ createGame('?','16:00','?'))+
+    createCard("05/12", "oitavas de final", createGame('?','12:00', '?')+ createGame('?','16:00','?'))+
+    createCard("06/12", "oitavas de final", createGame('?','12:00', '?')+ createGame('?','16:00','?'))+
+    createCard("09/12", "quartas de final", createGame('?','12:00', '?')+ createGame('?','16:00','?'))+
+    createCard("10/12", "quartas de final", createGame('?','12:00', '?')+ createGame('?','16:00','?'))+
+    createCard("13/12", "semifinais", createGame('?','16:00', '?'))+
+    createCard("14/12", "semifinais", createGame('?','16:00', '?'))+
+    createCard("17/12", "disputa pelo terceiro lugar", createGame('?','12:00', '?'))+
+    createCard("18/12", "final", createGame('?','12:00', '?'))
     
 
 
